273c963ce09eb1a6ef71acd16490344d85f56bd0
Seweryn Wawrzynowicz <sewerynwawrzynowicz@MacBook-Pro-Seweryn.local>
2023-03-11 17:31:11 +0100

    Edit post type

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,14 +14,7 @@ module.exports = [
       },
     },
   },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['*']
-    }
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
